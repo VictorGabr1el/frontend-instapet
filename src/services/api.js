1fe155514ea7a57b262e88cd33213a30558050a6
Victor Gabriel <vgbp21@gmail.com>
2023-04-25 21:03:48 -0300

    ainda tentando corrigir bug do modulo

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-export const Api = axios.create({
+const Api = axios.create({
   baseURL: "http://localhost:5000",
   headers: { "Content-Type": "application/json" },
 });
+
+export default Api;
